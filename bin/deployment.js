5ecd098f3fa569e15847052f3ca76bbfdfa9fca3
Filipe Oliveira <contato@fmoliveira.com.br>
2015-10-05 23:08:39 -0300

    Basic CLI interface.

---
+++ bin/deployment.js
@@ -0,0 +1,12 @@
+var program = require('commander'),
+	package = require('../package'),
+	deploy = require('../lib/deploy');
+
+program
+	.version(package.version)
+	.option('-d --deploy [id]', 'deploy website [id] now')
+	.parse(process.argv);
+
+if (program.id) {
+	deploy(program.id);
+}
